085224d428b21515c6c136f2e7181a440889ec3d
Ryan Williams <rwilliams@mla.org>
2019-02-28 09:41:28 -0500

    backend hooks for member search

--- gulpfile.js
+++ gulpfile.js
@@ -13,5 +13,5 @@ gulp.task('sass', function () {
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./sass/**/*.scss',  gulp.series('sass'));
 });
